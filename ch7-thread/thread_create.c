7d3b4b50ad98860ba57c0b0bf4399c5345f7faf1
robin <chou_robin@hotmail.com>
2016-08-24 20:09:48 +0800

    modified

--- ch7-thread/thread_create.c
+++ ch7-thread/thread_create.c
@@ -1,14 +1,6 @@
-/*************************************************************************
-	> File Name: thread_create.c
-	> Author: Robin
-	> Mail: chou_robin@163.com 
-	> Created Time: 2016年07月11日 星期一 17时49分21秒
- ************************************************************************/
-
 #include<stdio.h>
 #include<pthread.h>
 #include<unistd.h>
-
 
 void *th_func(void *arg)
 {
@@ -17,17 +9,13 @@ void *th_func(void *arg)
 	return NULL;
 }
 
-
-
 int main()
 {
-	pthread_t tid;
-	pthread_create(&tid, NULL, th_func, NULL);
-	printf("main tid: %x \n", (unsigned int)tid);
-	pthread_join(tid, NULL);//等待线程终止
+	pthread_t tid;                                  //
+	pthread_create(&tid, NULL, th_func, NULL);      //
+	printf("main tid: %x \n", (unsigned int)tid);   //
+	pthread_join(tid, NULL);                        //等待线程终止
 
-	return 0;
+	return 0;                                       //
 }
-
-
 
